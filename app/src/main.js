94717e5520af0190e07b8636e32f75fd9410a5bb
Rodrigo Carvalho <ro051186@hotmail.com>
2016-04-13 18:01:32 -0300

    Removendo

---
+++ app/src/main.js
@@ -0,0 +1,15 @@
+/// init Visual plugin
+$(document).ready(function(){
+		//ativa a acao scroll cada plugin
+
+	$('#fullpage').fullpage({
+		navigation:true,
+		menu:'#menu',
+		loopTop:true,
+		loopBottom:true
+	});
+
+
+});
+
+
---
+++ app/src/sweet.js
@@ -0,0 +1,11 @@
+
+$( "#front" ).click(function() {
+sweetAlert({
+	title: "Front-End", 
+    text: ("<span>Habilidades</span>"),
+  
+    html: true
+});
+});
+
+
